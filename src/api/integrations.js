2a656d23772cbc40157035b910f72edb3aefe90a
Matan Elkobi <189894787+MatanOps@users.noreply.github.com>
2025-03-09 00:11:49 +0200

    Update integrations.js

--- src/api/integrations.js
+++ src/api/integrations.js
@@ -1,24 +1,12 @@
-import { base44 } from './base44Client';
+import { apiClient } from './base44Client';
 
-
-
-
-export const Core = base44.integrations.Core;
-
-export const InvokeLLM = base44.integrations.Core.InvokeLLM;
-
-export const SendEmail = base44.integrations.Core.SendEmail;
-
-export const SendSMS = base44.integrations.Core.SendSMS;
-
-export const UploadFile = base44.integrations.Core.UploadFile;
-
-export const GenerateImage = base44.integrations.Core.GenerateImage;
-
-export const ExtractDataFromUploadedFile = base44.integrations.Core.ExtractDataFromUploadedFile;
-
-
-
+export const Core = apiClient.integrations.Core;
+export const InvokeLLM = apiClient.integrations.Core.InvokeLLM;
+export const SendEmail = apiClient.integrations.Core.SendEmail;
+export const SendSMS = apiClient.integrations.Core.SendSMS;
+export const UploadFile = apiClient.integrations.Core.UploadFile;
+export const GenerateImage = apiClient.integrations.Core.GenerateImage;
+export const ExtractDataFromUploadedFile = apiClient.integrations.Core.ExtractDataFromUploadedFile;
 
 
 
