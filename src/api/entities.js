b4f231982802236165b92e6a62690add5abedb18
Matan Elkobi <189894787+MatanOps@users.noreply.github.com>
2025-03-09 00:10:52 +0200

    Update entities.js

--- src/api/entities.js
+++ src/api/entities.js
@@ -1,13 +1,6 @@
-import { base44 } from './base44Client';
+iimport { apiClient } from './base44Client';
 
-
-export const ApiTest = base44.entities.ApiTest;
-
-export const TestSuite = base44.entities.TestSuite;
-
-export const ApiMonitor = base44.entities.ApiMonitor;
-
-
-
-// auth sdk:
-export const User = base44.auth;
+export const ApiTest = apiClient.entities.ApiTest;
+export const TestSuite = apiClient.entities.TestSuite;
+export const ApiMonitor = apiClient.entities.ApiMonitor;
+export const User = apiClient.auth;
